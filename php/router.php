ca7b23e50bda527126a725a190dc4804e788fb41
Shovan Mondal <78010171+shovan04@users.noreply.github.com>
2022-09-07 11:01:46 +0530

    Update router.php

--- php/router.php
+++ php/router.php
@@ -5,7 +5,7 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $url = $_SERVER['REQUEST_URI'];
-$url = str_replace('/project-Le/edu-com','',$url);
+$url = str_replace($_ENV['REPLACE_URL'],'',$url);
 $arr = explode('/',$url);
 
 // echo 'router page        ';
